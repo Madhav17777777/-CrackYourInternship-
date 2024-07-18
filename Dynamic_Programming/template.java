0b44d27f2d8719c6bee893d1f89e96e3296ddd90
Madhav7 <142514622+Madhav17777777@users.noreply.github.com>
2024-07-18 11:39:13 +0530

    template of java

---
+++ Dynamic_Programming/template.java
@@ -0,0 +1,11 @@
+package Dynamic_Programming;
+
+import java.util.*;
+
+public class template {
+  public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+
+    sc.close();
+  }
+}
