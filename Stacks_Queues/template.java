0b12bb56712ad19b128a38e4cd0a770a343a8334
Madhav7 <142514622+Madhav17777777@users.noreply.github.com>
2024-07-20 18:22:20 +0530

    template for stacks

---
+++ Stacks_Queues/template.java
@@ -0,0 +1,11 @@
+package Stacks_Queues;
+
+import java.util.*;
+
+public class template {
+  public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+
+    sc.close();
+  }
+}
